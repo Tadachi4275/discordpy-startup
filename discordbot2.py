a73d6f9f4194193c249e34559de7962a0793504a
Tadachi4275 <tadachi4275@gmail.com>
2020-11-03 17:55:44 +0900

    募集用botソースコード追加

--- discordbot2.py
+++ discordbot2.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 import traceback
 import asyncio
+import datetime as dt
 
 token = os.environ['DISCORD_BOT_TOKEN']
 client = commands.Bot(command_prefix='.')
@@ -17,11 +18,19 @@ async def on_ready() :
     print('------')
 
 @client.command()
-async def rect(ctx, about = "募集", cnt = 4, settime = 10.0) :
-    cnt, settime = int(cnt), float(settime)
+async def rect(ctx, about = "募集", settime = '2030-11-2,22:07:00') :
+    cnt = 0
+    datetime_UTC = dt.datetime.strptime(settime, '%Y-%m-%d,%H:%M:%S')
+    nowTime_UTC = dt.datetime.now()
+    nowTime_UTC = nowTime_UTC.replace(microsecond=0)
+    td = datetime_UTC - nowTime_UTC
+    print('Start Time: {0}\n'.format(nowTime_UTC))
+    print('DeadLine: {0}\n'.format(datetime_UTC))
+    td = float(td.seconds)
+    print('Time Difference(initial)={0}(sec)\n'.format(td))
     reaction_member = [">>>"]
     test = discord.Embed(title=about, colour=0x1e90ff)
-    test.add_field(name=f"あと{cnt}人　募集中\n", value=None, inline=True)
+    test.add_field(name=f"{datetime_UTC}まで募集中。今{cnt}人。\n", value=None, inline=True)
     msg = await ctx.send(embed=test)
     #await ctx.send(type(msg))
 
@@ -35,35 +44,32 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0) :
         else:
             return emoji == '⏫' or emoji == '✖'
 
-    while len(reaction_member)-1 <= cnt:
+    while 1:
         try:
-            reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
+            nowTime_Poll = dt.datetime.now()
+            print(nowTime_Poll)
+            print('Time Difference(Polling)={0}(sec)\n'.format(td))
+            reaction, user = await client.wait_for('reaction_add', timeout=td, check=check)
         except asyncio.TimeoutError:
-            await ctx.send('残念、人が足りなかったようだ...')
+            nowTime_Det = dt.datetime.now()
+            print(nowTime_Det)
+            await ctx.send('締め切りｳｪｰｲ!!!!')
             break
         else:
             print(str(reaction.emoji))
             if str(reaction.emoji) == '⏫':
                 reaction_member.append(user.name)
-                cnt -= 1
+                cnt += 1
                 test = discord.Embed(title=about,colour=0x1e90ff)
-                test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
+                test.add_field(name=f"{datetime_UTC}まで募集中。今{cnt}人。\n", value='\n'.join(reaction_member), inline=True)
                 await msg.edit(embed=test)
-
-                if cnt == 0:
-                    test = discord.Embed(title=about,colour=0x1e90ff)
-                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                    await msg.edit(embed=test)
-                    finish = discord.Embed(title=about,colour=0x1e90ff)
-                    finish.add_field(name="おっと、メンバーがきまったようだ",value='\n'.join(reaction_member), inline=True)
-                    await ctx.send(embed=finish)
-
+                
             elif str(reaction.emoji) == '✖':
                 if user.name in reaction_member:
                     reaction_member.remove(user.name)
-                    cnt += 1
+                    cnt -= 1
                     test = discord.Embed(title=about,colour=0x1e90ff)
-                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
+                    test.add_field(name=f"{datetime_UTC}まで募集中。今{cnt}人。\n", value='\n'.join(reaction_member), inline=True)
                     await msg.edit(embed=test)
                 else:
                     pass
